c9b5372ad2463fe491252865fff231dbefbdc240
Gianluca Giangreco <giobbe68@hotmail.com>
2016-11-07 00:40:02 +0100

    Add files via upload

---
+++ f_Azzeramento_massimi_meter.ino
@@ -0,0 +1,85 @@
+void Azzeramento()
+{
+//    -------- Menu 2 - Azzeramento --------
+lcd.print("   AZZERARE?    ");
+t1=millis();
+while(digitalRead(5)==LOW) // Attende che venga lasciato il pulsante.
+{if(millis()-t1>1000) {Bip(); lcd.clear(); massimi(); return;}}  // Premuto per almeno 1 secondo (3,2 totali) salta a massimi(), poi ritorna ed esce.
+t1=millis();
+while(digitalRead(5)==HIGH) // Continua a leggere l'encoder finché non premo
+  {
+  encoder();
+  if(E!=0){Azz+=E; E=0; t1=millis(); delay(20);}
+  if(Azz>1) Azz=1;
+  if(Azz<0) Azz=0;
+  
+  lcd.setCursor(7,1);
+  if(Azz==0){lcd.print("No");}
+  else{lcd.print("Si");}
+  if(millis()-t1>4999) return; // Dopo 5 secondi di inattività esce.
+  }
+  
+if (Azz==1) {Azz=0; Azzera();} // Azzera
+else Bip();
+} // END Azzeramento()
+
+void Azzera()
+{Biiip(); cp=0; tempo=0; m=1; m1=1; t3=0; DA=0; DB=0; D=0; DMAX=0; for(m1=1; m1<61; m1++){C[m1]=0;}}
+ // END Azzera()
+
+void massimi()
+{
+lcd.print("    Massimi     ");  
+while(digitalRead(5)==LOW) // Attende che venga lasciato il pulsante.
+{if(millis()-t1>1000) {Bip(); lcd.clear(); meter(); return;}}  // Premuto per almeno 1 secondo (3,2 totali) salta a massimi(), poi ritorna ed esce.
+t1=millis();
+Mask(); lcd.setCursor(11,0); lcd.print("max"); lcd.setCursor(13,1); lcd.print("max"); 
+lcd.setCursor(0,0); if(DMAX*60>ownbcpm) Imp=DMAX*60-ownbcpm; else Imp=0; printImp(); // D è in cps; printImp vuole cpm.
+lcd.setCursor(0,1); Rad=float(Imp)/sens; printRad(); // uSv/h in virgola mobile. 
+delay(300);
+while(digitalRead(5)==HIGH){if(millis()-t1>9999) return;} // Attende che venga premuto il pulsante, ma dopo 10 secondi comunque esce.
+} // END massimi()
+
+void meter()
+{
+lcd.print("       LED      ");  
+while(digitalRead(5)==LOW) // Attende che venga lasciato il pulsante.
+{if(millis()-t1>1000) {Bip(); lcd.clear(); retroillum(); return;}}  // Premuto per almeno 1 secondo (3,2 totali) salta a massimi(), poi ritorna ed esce.
+lcd.setCursor(0,1); lcd.print(" Off   Dot   Bar");
+t1=millis();
+while(digitalRead(5)==HIGH) // Continua a leggere l'encoder finché non premo
+  {
+  encoder();
+  if(E!=0){LED+=E; E=0; t1=millis(); delay(20);}
+  if(LED>2) LED=2;
+  if(LED<0) LED=0;
+
+  if(LED==0)      {lcd.setCursor(0,1); lcd.print("x"); lcd.setCursor(6,1); lcd.print(" "); lcd.setCursor(12,1); lcd.print(" ");}
+  else if(LED==1) {lcd.setCursor(0,1); lcd.print(" "); lcd.setCursor(6,1); lcd.print("x"); lcd.setCursor(12,1); lcd.print(" ");}
+  else if(LED==2) {lcd.setCursor(0,1); lcd.print(" "); lcd.setCursor(6,1); lcd.print(" "); lcd.setCursor(12,1); lcd.print("x");}
+  
+  if(millis()-t1>4999) return; // Dopo 5 secondi di inattività esce.
+  }
+if(LED!=EEPROM.read(10)) {EEPROM.update(10,LED); Biip(); lcd.setCursor(12,1); lcd.print(" SET!"); delay(500);}
+else Bip();  
+}
+
+void retroillum()
+{
+lcd.print(" Retroillumin.  ");  
+while(digitalRead(5)==LOW); // Attende che venga lasciato il pulsante.
+delay(300);
+t1=millis();
+while(digitalRead(5)==HIGH) // Continua a leggere l'encoder finché non premo
+  {
+  encoder();
+  if(E!=0){LCD+=E; E=0; t1=millis(); delay(20);}
+  if(LCD>2) LCD=2;
+  if(LCD<1) LCD=1;
+
+  lcd.setCursor(6,1); if(LCD==1) lcd.print("On  "); else lcd.print("Auto");
+  if(millis()-t1>4999) return; // Dopo 5 secondi di inattività esce.
+  }
+if(LCD!=EEPROM.read(11)) {EEPROM.update(11,LCD); Biip(); lcd.setCursor(12,1); lcd.print(" SET!"); delay(500);}
+else Bip();  
+}
