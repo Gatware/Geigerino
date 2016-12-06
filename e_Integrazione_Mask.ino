797a5be8dc6d7ded527e984b2f90b80fc70dcaa6
Gianluca Giangreco <giobbe68@hotmail.com>
2016-12-06 09:13:13 +0100

    Update e_Integrazione_Mask.ino

--- e_Integrazione_Mask.ino
+++ e_Integrazione_Mask.ino
@@ -20,7 +20,7 @@ while(digitalRead(5)==HIGH) // Continua a leggere l'encoder finché non premo
   else lcd.print("Continuo   "); // Se Ti=70, integra continuamente a tempo infinito.
   if(millis()-t1>4999) return; // Dopo 5 secondi di inattività esce.
   }
-if(Ti!=Tio) lcd.setCursor(5,1); lcd.print("ZERO"); // Se il tempo impostato è diverso dal precedente, segnala che azzera.
+if(Ti!=Tio) lcd.setCursor(4,1); lcd.print(" ZERO"); // Se il tempo impostato è diverso dal precedente, segnala che azzera.
 if(Ti!=EEPROM.read(0)) {EEPROM.update(0,Ti); Biip(); lcd.setCursor(10,1); lcd.print("  SET!"); delay(500);}
 else Bip();
 if(Ti!=Tio) {Biiip(); Azzera();} // Se il tempo impostato è minore del precedente, azzera e fa Biiip. Mettendolo 3 righe più su,
